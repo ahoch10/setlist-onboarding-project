ace59f06c9c6b6f75a6335ead78a5bac56c6feab
anathochberg <anathochberg@gmail.com>
2021-11-18 17:09:50 -0500

    added model, GET and POST routes

---
+++ server/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:12345@localhost:5432/setlist"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    key = db.Column(db.String(10))
+    instrumentation = db.Column(db.Text)
+    notes = db.Column(db.Text)
+
+    def __init__(self, title, key, instrumentation, notes):
+        self.title=title
+        self.key=key
+        self.instrumentation=instrumentation
+        self.notes=notes
+
+# GET route to get all songs
+@app.route('/', methods=["GET"])
+def home():
+    try:
+        songs = Song.query.all()
+        results = [ {
+            "id": song.id,
+            "title": song.title,
+            "instrumentation": song.instrumentation,
+            "key": song.key,
+            "notes": song.notes
+        } for song in songs]
+        return jsonify({"songs": results})
+    except Exception as e:
+        return(str(e))
+
+# POST route to add a song
+@app.route('/', methods=["POST"])
+def add_song():
+    title=request.args.get('title')
+    key=request.args.get('key')
+    instrumentation=request.args.get('instrumentation')
+    notes=request.args.get('notes')
+    try:
+        song=Song(
+                title=title,
+                key=key,
+                # instrumentation=instrumentation,
+                notes=notes
+        )
+        db.session.add(song)
+        db.session.commit()
+        return "Song added"
+    except Exception as e:
+        return(str(e))
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
