e99eba337ff6f9e42921339875b667f412c9510a
anathochberg <anathochberg@gmail.com>
2021-11-19 15:04:42 -0500

    added view of all songs in setlist in front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(()=> {
+    fetch("/songs").then(
+      res => res.json()).then(data => {
+      console.log("data", data)
+      setSongs(data.songs)})
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Setlist</h1>
+      <table>
+        <tbody>
+        <tr className="table-row">
+          <th>Song</th>
+          <th>Key</th>
+          <th>Instrumentation</th>
+          <th>Notes</th>
+        </tr>
+        {songs.map((song, i)=>{
+          return <tr key={i} className="table-row">
+              <td>{song.title}</td>
+              <td>{song.key}</td>
+              <td>{song.instrumentation}</td>
+              <td>{song.notes}</td>
+            </tr>
+        })}
+        </tbody>
+      </table>
     </div>
   );
 }
