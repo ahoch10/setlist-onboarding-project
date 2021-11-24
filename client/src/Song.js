2311fcc42ca2e12d16fb14d57cccc4c3b3175f6f
anathochberg <anathochberg@gmail.com>
2021-11-24 13:01:33 -0500

    added form for updating songs

--- client/src/Song.js
+++ client/src/Song.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Song = (props) => {
+  const [updateSong, setUpdateSong] = useState(false)
+  const [updatedSong, setUpdatedSong] = useState ({ title: "", key: "", instrumentation: "", notes: "" });
 
   const song = props.song;
 
@@ -12,13 +14,34 @@ const Song = (props) => {
     fetch(`songs/${song.id}`, options)
   }
 
-  return <tr key={song.id} className="table-row">
+  const handleUpdate = (event) => {
+    const options = {
+      method: 'PUT',
+      body: updatedSong
+    }
+
+    fetch(`songs/${song.id}`, options)
+  }
+
+  if (updateSong) {
+    return (<tr>
+      <td><input type="input" placeholder={song.title} /></td>
+      <td><input type="input" placeholder={song.key} /></td>
+      <td><input type="input" placeholder={song.instrumentation} /></td>
+      <td><input type="input" placeholder={song.notes} /></td>
+      <td><button type="button" onClick={handleDelete}>Delete</button></td>
+      <td><button type="button" onClick={handleUpdate}>Update</button></td>
+    </tr>)
+  } else {
+  return (  <tr key={song.id} className="table-row">
               <td>{song.title}</td>
               <td>{song.key}</td>
               <td>{song.instrumentation}</td>
               <td>{song.notes}</td>
               <td><button type="button" onClick={handleDelete}>Delete</button></td>
+              <td><button type="button" onClick={()=>setUpdateSong(true)}>Update</button></td>
             </tr>
+  ) }
 
 }
 
