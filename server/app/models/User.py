2aeafce3c8bfe84f0e3daaac145de67438025b8d
anathochberg <anathochberg@gmail.com>
2021-12-10 10:27:26 -0500

    updated User model, removing token info fields

--- server/app/models/User.py
+++ server/app/models/User.py
@@ -4,7 +4,3 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(300), nullable=False)
-    access_token = db.Column(db.Text, nullable=False)
-    token_type = db.Column(db.String(20), nullable=False)
-    expires_at = db.Column(db.Integer, nullable=False)
-    refresh_token = db.Column(db.Text, nullable=False)
