87b0a8b27e7f50ff11deef1c1b3f3bb062b532a5
anathochberg <anathochberg@gmail.com>
2021-12-14 14:54:36 -0500

    added requires_login decorator to all setlist routes

--- server/app/blueprints/setlist_blueprint.py
+++ server/app/blueprints/setlist_blueprint.py
@@ -1,11 +1,24 @@
 from flask import Flask, request, jsonify, Blueprint, session
 from app.models.Setlist import Setlist
+from app.models.User import User
 from app.extensions import db
 
 
 setlists = Blueprint("setlists", __name__)
 
+def requires_login(func):
+    def wrapper(*args, **kwargs):
+        user_email = session.get("user_email")
+        if user_email is None:
+            return redirect("/login")
+        user = User.query.filter_by(email = user_email).one_or_none()
+        if user is None:
+            return redirect("/login")
+        return func(user, *args, **kwargs)
+    return wrapper
+
 # GET route to get all setlists
+@requires_login
 @setlists.route("/setlists", methods=["GET"])
 def all_setlists():
     setlists = Setlist.query.all()
@@ -16,16 +29,15 @@ def all_setlists():
 
 
 # POST route to add a setlist
+@requires_login
 @setlists.route("/setlists", methods=["POST"])
-def add_setlist():
-    user_email = session.get("user_email")
-    if user_email is None:
-        return redirect("/login")
+def add_setlist(user:User):
+
     setlist_data = request.get_json()
 
     title = setlist_data.get("title")
     date = setlist_data.get("date")
-    user_id = setlist_data.get("user_id")
+    user_id = user.id
 
     setlist = Setlist(title=title, date=date, user_id=user_id)
     db.session.add(setlist)
@@ -34,8 +46,9 @@ def add_setlist():
 
 
 # DELETE route to delete a setlist
+@requires_login
 @setlists.route("/setlists/<int:id>", methods=["DELETE"])
-def delete_setlist(id: int):
+def delete_setlist(user:User, id: int):
     setlist = Setlist.query(id)
     db.session.delete(setlist)
     db.session.commit()
@@ -44,6 +57,7 @@ def delete_setlist(id: int):
 
 
 # PUT route to update a setlist
+@requires_login
 @setlists.route("/setlists/<int:id>")
 def update_setlist(id: int):
     setlist = Setlist.query.get(id)
@@ -55,3 +69,4 @@ def update_setlist(id: int):
     db.session.commit()
 
     return "Setlist updated"
+
