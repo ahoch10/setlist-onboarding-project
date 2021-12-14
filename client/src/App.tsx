db65ffc5455c7dc8e9c56fc5b9cdf520e94b0fa4
anathochberg <anathochberg@gmail.com>
2021-12-14 17:31:47 -0500

    front end: new home page with login button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,69 +6,14 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import { SongWithId } from './types'
 
 const App: FC = () => {
-  const { songs, setSongs, addSong, updateSong, deleteSong, reorderSongs } =
-    useSong()
-
-  const handleDragEnd = (result) => {
-    if (!result.destination) return
-    const updatedSongs: SongWithId[] = Array.from(songs)
-    const [reorderedSong]: SongWithId = updatedSongs.splice(
-      result.source.index,
-      1
-    )
-    updatedSongs.splice(result.destination.index, 0, reorderedSong)
-    setSongs(updatedSongs)
-
-    const reorderedSongs: SongWithId[] = updatedSongs.map((song, index) => {
-      song.order_index = index
-      return song
-    })
-
-    reorderSongs(reorderedSongs)
-  }
 
   return (
     <div>
-      <h1>My Setlist</h1>
-      <DragDropContext onDragEnd={handleDragEnd}>
-        <div className="table">
-          <div className="table-row headers">
-            <div>Song</div>
-            <div>Key</div>
-            <div>Instrumentation</div>
-            <div>Notes</div>
-            <div></div>
-            <div></div>
-          </div>
-          <Droppable droppableId="songs">
-            {(provided) => (
-              <div
-                className="songs"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {songs.map((song, i) => {
-                  return (
-                    <Song
-                      song={song}
-                      key={i}
-                      index={i}
-                      songs={songs}
-                      setSongs={setSongs}
-                      updateSong={updateSong}
-                      deleteSong={deleteSong}
-                    />
-                  )
-                })}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-          <AddSong setSongs={setSongs} songs={songs} addSong={addSong} />
-        </div>
-      </DragDropContext>
+    <h1 className="welcome">Welcome</h1>
+    <button type="button">Login with Spotify</button>
     </div>
   )
+
 }
 
 export default App
---
+++ client/src/Setlist.tsx
@@ -0,0 +1,75 @@
+
+import React, { FC } from 'react'
+import AddSong from './AddSong'
+import Song from './Song'
+import { useSong } from './useSong'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import { SongWithId } from './types'
+
+const Setlist: FC = () => {
+  const { songs, setSongs, addSong, updateSong, deleteSong, reorderSongs } =
+    useSong()
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return
+    const updatedSongs: SongWithId[] = Array.from(songs)
+    const [reorderedSong]: SongWithId = updatedSongs.splice(
+      result.source.index,
+      1
+    )
+    updatedSongs.splice(result.destination.index, 0, reorderedSong)
+    setSongs(updatedSongs)
+
+    const reorderedSongs: SongWithId[] = updatedSongs.map((song, index) => {
+      song.order_index = index
+      return song
+    })
+
+    reorderSongs(reorderedSongs)
+  }
+
+  return (
+    <div>
+      <h1>My Setlist</h1>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div className="table">
+          <div className="table-row headers">
+            <div>Song</div>
+            <div>Key</div>
+            <div>Instrumentation</div>
+            <div>Notes</div>
+            <div></div>
+            <div></div>
+          </div>
+          <Droppable droppableId="songs">
+            {(provided) => (
+              <div
+                className="songs"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {songs.map((song, i) => {
+                  return (
+                    <Song
+                      song={song}
+                      key={i}
+                      index={i}
+                      songs={songs}
+                      setSongs={setSongs}
+                      updateSong={updateSong}
+                      deleteSong={deleteSong}
+                    />
+                  )
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <AddSong setSongs={setSongs} songs={songs} addSong={addSong} />
+        </div>
+      </DragDropContext>
+    </div>
+  )
+}
+
+export default Setlist
