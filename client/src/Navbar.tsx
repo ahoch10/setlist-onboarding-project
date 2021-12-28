49289968405d43624d53385b43099a960e2464b5
anathochberg <anathochberg@gmail.com>
2021-12-28 16:52:07 -0500

    fixed logout function to reload page after logout

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -3,11 +3,17 @@ import React from 'react'
 const Navbar = () => {
   const logout = () => {
     fetch('/logout')
-      .then((res) => console.log(res))
+      .then((res) => window.location.reload())
       .catch((err) => console.log('error logging out', err))
   }
 
-  return <div className="navbar" onClick={logout}>Logout</div>
+  return (
+    <div className="navbar">
+      <div className="logout" onClick={logout}>
+        Logout
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
